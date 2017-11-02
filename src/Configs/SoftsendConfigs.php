3ea6bf9863d9eca554b5210bade6679a6d361da8
igorwanbarros <igorwanbarros@gmail.com>
2017-11-02 14:53:00 -0300

    commit inicial

---
+++ src/Configs/SoftsendConfigs.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Softcomtecnologia\SoftsendClient\Configs;
+
+class SoftsendConfigs
+{
+    const URL_DOMAIN = 'http://localhost/softcom/softcom-app-envios/public/softauth';
+
+    const URL_STORE_CLIENT = 'add-client';
+}
