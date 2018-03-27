572fbe65407d45c46723cc8b171a7b185df6102a
wanderley <igorwanbarros@gmail.com>
2018-03-27 17:03:15 -0300

    atualizando classe SendEmail com trait SendRequestSupport.

--- src/Supports/SendEmail.php
+++ src/Supports/SendEmail.php
@@ -2,12 +2,18 @@
 
 namespace Softcomtecnologia\SoftsendClient\Supports;
 
-use Softcomtecnologia\Api\Exceptions\InvalidResponseException;
 use Softcomtecnologia\SoftsendClient\Configs\SoftsendConfigs;
+use Softcomtecnologia\Api\Exceptions\InvalidResponseException;
 use Softcomtecnologia\SoftsendClient\Contracts\SupportAbstract;
 
 class SendEmail extends SupportAbstract
 {
+
+    /**
+     * @var string
+     */
+    protected $urlSend = SoftsendConfigs::URL_SEND_EMAIL;
+
 
     /**
      * @param array  $data
@@ -15,35 +21,21 @@ class SendEmail extends SupportAbstract
      * @param string $clientSecret
      * @param string $domain
      */
-    public function __construct(array $data = [], $clientId = '', $clientSecret = '', $domain = '')
-    {
-        $optionsProvider = get_defined_vars();
-
-        if (!$optionsProvider['domain']) {
-            $optionsProvider['domain'] = SoftsendConfigs::URL_DOMAIN;
-        }
-
-        parent::__construct($optionsProvider);
+    public function __construct(
+        array $data = [],
+        $clientId = '',
+        $clientSecret = '',
+        $domain = SoftsendConfigs::URL_DOMAIN
+    ) {
+        parent::__construct(get_defined_vars());
     }
 
 
+    /**
+     * @var mixed
+     */
     public function support()
     {
-        if ($this->getDebugMode()) {
-            return $this->responseToDebugMode();
-        }
-
-        try {
-            $response = $this->provider->post(
-                $this->getToken(),
-                SoftsendConfigs::URL_SEND_EMAIL,
-                $this->optionsProvider
-            );
-        } catch (InvalidResponseException $e) {
-            $response = $e->getRequestParamsFromString();
-        }
-
-        return $this->prepareResponse($response);
+        return $this->sendSupportPost();
     }
-
 }
